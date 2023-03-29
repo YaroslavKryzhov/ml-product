6de6c35d900c1923a5302838feb795425ea2bc17
Ilia <bountyhntr1337@gmail.com>
2023-03-29 12:06:33 +0300

    feature(models) - regression models params pt.1
    added regression models params

--- server/ml_api/apps/ml_models/utils/regression_models_params.py
+++ server/ml_api/apps/ml_models/utils/regression_models_params.py
@@ -29,3 +29,175 @@ class DecisionTreeRegressorParameters(BaseModel):  # ready
         if value < 1:
             raise ValueError('min_samples_leaf must be 1 or greater.')
         return value
+
+class BaggingRegressorParameters(BaseModel):
+    n_estimators: int = 10
+    max_samples: Union[int, float] = 1
+    max_features: Union[int, float] = 1
+    bootstrap: bool = True
+    bootstrap_features: bool = False
+    n_jobs: Optional[int] = N_JOBS 
+
+    @validator("n_estimators")
+    def validate_n_estimators(cls, value):
+        if value < 2:
+            raise ValueError('n_estimators must be 2 or greater.')
+        return value
+
+    @validator("max_samples")
+    def validate_max_samples(cls, value):
+        if value < 1:
+            raise ValueError('max_samples must be 1 or greater.')
+        return value
+
+    @validator("max_features")
+    def validate_max_features(cls, value):
+        if value < 1:
+            raise ValueError('max_features must be 1 or greater.')
+        return value
+
+class ExtraTreeRegressorParameters(BaseModel):
+    n_estimators: int = 100
+    criterion: Literal['squared_error', 'absolute_error', 'friedman_mse', 'poisson'] = 'squared_error'
+    max_depth: Optional[int] = None
+    min_samples_split: int = 2
+    min_samples_leaf: int = 1
+    bootstrap: bool = False
+    n_jobs: Optional[int] = N_JOBS 
+
+    @validator("n_estimators")
+    def validate_n_estimators(cls, value):
+        if value < 2:
+            raise ValueError('n_estimators must be 2 or greater.')
+        return value
+
+    @validator("max_depth")
+    def validate_max_depth(cls, value):
+        if value <= 0:
+            raise ValueError('max_depth must be greater than zero.')
+        return value
+
+    @validator("min_samples_split")
+    def validate_min_samples_split(cls, value):
+        if value < 2:
+            raise ValueError('min_samples_split must be 2 or greater.')
+        return value
+
+    @validator("min_samples_leaf")
+    def validate_min_samples_leaf(cls, value):
+        if value < 1:
+            raise ValueError('min_samples_leaf must be 1 or greater.')
+        return value
+
+class GradientBoostingRegressorParameters(BaseModel):
+    loss: Literal['squared_error', 'absolute_error', 'huber', 'quantile'] = 'squared_error'
+    learning_rate: float = 0.1
+    n_estimators: int = 100
+    subsample: float = 1
+    criterion: Literal['friedman_mse', 'squared_error'] = 'friedman_mse'
+    min_samples_split: int = 2
+    max_depth: int = 3
+
+    @validator("learning_rate")
+    def validate_learning_rate(cls, value):
+        if value <= 0:
+            raise ValueError('learning_rate must be greater than zero.')
+        return value
+
+    @validator("n_estimators")
+    def validate_n_estimators(cls, value):
+        if value < 2:
+            raise ValueError('n_estimators must be 2 or greater.')
+        return value
+
+    @validator("subsample")
+    def validate_subsample(cls, value):
+        if value > 1 or value <= 0:
+            raise ValueError("subsample must be in (0, 1]")
+        return value
+
+    @validator("min_samples_split")
+    def validate_min_samples_split(cls, value):
+        if value < 2:
+            raise ValueError('min_samples_split must be 2 or greater.')
+        return value
+
+    @validator("max_depth")
+    def validate_max_depth(cls, value):
+        if value <= 0:
+            raise ValueError('max_depth must be greater than zero.')
+        return value
+
+class RandomForestRegressorParameters(BaseModel):
+    n_estimators: int = 100
+    criterion: Literal['squared_error', 'absolute_error', 'friedman_mse', 'poisson'] = 'squared_error'
+    max_depth: int = None
+    min_samples_split: int = 2
+    min_samples_leaf: int = 1
+    bootstrap: bool = True
+
+    @validator("n_estimators")
+    def validate_n_estimators(cls, value):
+        if value < 2:
+            raise ValueError('n_estimators must be 2 or greater.')
+        return value
+
+    @validator("max_depth")
+    def validate_max_depth(cls, value):
+        if value <= 0:
+            raise ValueError('max_depth must be greater than zero.')
+        return value
+
+    @validator("min_samples_split")
+    def validate_min_samples_split(cls, value):
+        if value < 2:
+            raise ValueError('min_samples_split must be 2 or greater.')
+        return value
+
+    @validator("min_samples_leaf")
+    def validate_min_samples_leaf(cls, value):
+        if value < 1:
+            raise ValueError('min_samples_leaf must be 1 or greater.')
+        return value
+
+class LinearRegressionParameters(BaseModel):
+    fit_intercept: bool = True
+    copy_X: bool = True
+    n_jobs: Optional[int] = N_JOBS
+    positive: bool = False
+
+class SGDRegressorParameters(BaseModel):
+    loss: Literal['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'] = 'hinge'
+    penalty: Optional[Literal['l2', 'l1', 'elasticnet']] = 'l2'
+    alpha: float = 0.0001
+    l1_ratio: float = 0.15
+    fit_intercept: bool = True
+    max_iter: int = 1000
+    shuffle: bool = True
+    epsilon: float = 0.1
+    n_jobs: Optional[int] = N_JOBS
+    learning_rate: Literal['constant', 'optimal', 'invscaling', 'adaptive'] = 'invscaling'
+
+    @validator("alpha")
+    def validate_alpha(cls, value):
+        if value < 0:
+            raise ValueError('alpha must be non-negative.')
+        return value
+
+    @validator("l1_ratio")
+    def validate_l1_ratio(cls, value):
+        if value < 0 or value > 1:
+            raise ValueError('l1_ratio must be in [0, 1].')
+        return value
+
+    @validator("max_iter")
+    def validate_max_iter(cls, value):
+        if value <= 0:
+            raise ValueError('max_iter must be greater than zero.')
+        return value
+
+    @validator("epsilon")
+    def validate_epsilon(cls, value):
+        if value < 0:
+            raise ValueError('epsilon must be non-negative.')
+        return value
--- server/ml_api/apps/ml_models/utils/regression_searchers.py
+++ server/ml_api/apps/ml_models/utils/regression_searchers.py
@@ -6,4 +6,33 @@ REGRESSION_CONFIG = {
         'criterion': hp.choice(label='criterion', options=['gini', 'entropy', 'squared_error', 'friedman_mse', 'absolute_error', 'poisson']),
         'max_depth': hp.randint(label='max_depth', low=3, high=30),
     },
+    'BaggingRegressor': {
+        'n_estimators': hp.randint(label="n_estimators", low=10, high=100),
+    },
+    'ExtraTreeRegressor': {
+        'n_estimators': hp.randint(label="n_estimators", low=50, high=300),
+        'criterion': hp.choice(label='criterion', options=['squared_error', 'absolute_error', 'friedman_mse', 'poisson']),
+        'max_depth': hp.randint(label='max_depth', low=3, high=20),
+    },
+    'GradientBoostringRegressor': {
+        'loss': hp.choice(label='loss', options=['squared_error', 'absolute_error', 'huber', 'quantile']),
+        'learning_rate': hp.uniform('learning_rate', 1e-6, 1),
+        'n_estimators': hp.randint(label="n_estimators", low=50, high=300),
+        'criterion': hp.choice(label='criterion', options=['friedman_mse', 'squared_error']),
+        'max_depth': hp.randint('max_depth', 3, 20),
+    },
+    'RandomForestRegressor': {
+        'n_estimators': hp.randint(label='n_estimators', low=50, high=300),
+        'criterion': hp.choice(label='criterion', options=['squared_error', 'absolute_error', 'friedman_mse', 'poisson']),
+        'max_depth': hp.randint(label='max_depth', low=3, high=20),
+    },
+    'LinearRegression': {
+    },
+    'SGDRegressor': {
+       'loss': hp.choice(label='loss', options=['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive']),
+       'penalty': hp.choice(label='penalty', options=['l2', 'l1', 'elasticnet', 'none']),
+       'alpha': hp.uniform('alpha', 1e-6, 1e-3),
+       'l1_ratio': hp.uniform('l1_ratio', 0.05, 0.95),
+       'learning_rate': hp.choice(label='learning_rate', options=['constant', 'optimal', 'invscaling', 'adaptive']),
+    },
 }
