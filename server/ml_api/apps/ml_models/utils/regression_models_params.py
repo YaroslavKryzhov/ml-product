e53ca864b5c188f88055f9401717eb2b3d8267d4
Егор Гуляев <55408625+Justegoriy@users.noreply.github.com>
2023-03-29 12:12:00 +0300

    feature(models) - regression models params pt.2
    * add regression + regr params

--- server/ml_api/apps/ml_models/utils/regression_models_params.py
+++ server/ml_api/apps/ml_models/utils/regression_models_params.py
@@ -1,4 +1,4 @@
-from typing import Union, Literal, Optional, Tuple
+from typing import Union, Literal, Optional, List
 
 from pydantic import BaseModel, validator
 
@@ -30,13 +30,162 @@ class DecisionTreeRegressorParameters(BaseModel):  # ready
             raise ValueError('min_samples_leaf must be 1 or greater.')
         return value
 
+
+class AdaBoostRegressorParameters(BaseModel):
+    loss: Literal['linear', 'square', 'exponential'] = 'linear'
+    learning_rate: float = 1.0
+    n_estimators: int = 50
+
+    @validator("learning_rate")
+    def validate_learning_rate(cls, value):
+        if value <= 0:
+            raise ValueError('learning_rate must be greater than zero.')
+        return value
+
+    @validator("n_estimators")
+    def validate_n_estimators(cls, value):
+        if value < 2:
+            raise ValueError('n_estimators must be 2 or greater.')
+        return value
+
+
+class KNeighborsRegressorParameters(BaseModel):
+    n_neighbors : int = 5
+    weights: Literal['uniform', 'distance'] = 'uniform'
+    algorithm : Literal['auto', 'ball_tree', 'kd_tree', 'brute'] = 'auto'
+    leaf_size: int = 30
+    p: int = 2
+    n_jobs: Optional[int] = N_JOBS
+
+    @validator("n_neighbors")
+    def validate_n_neighbors(cls, value):
+        if value <= 0:
+            raise ValueError('n_neighbors must be greater than zero.')
+        return value
+
+    @validator("leaf_size")
+    def validate_leaf_size(cls, value):
+        if value < 0:
+            raise ValueError('leaf_size must be 1 or greater.')
+        return value
+
+    @validator("p")
+    def validate_p(cls, value):
+        if value < 0:
+            raise ValueError('p must be 1 or greater.')
+        return value
+
+
+class LinearSVRParameters(BaseModel):
+    epsilon: float = 0.0
+    tol: float = 1e-4
+    C: float = 1.0
+    loss: Literal['epsilon_insensitive', 'squared_epsilon_insensitive'] = 'epsilon_insensitive'
+    fit_intercept: bool = True
+    dual: bool = True
+
+    @validator("epsilon")
+    def validate_epsilon(cls, value):
+        if value < 0:
+            raise ValueError('epsilon must be 0 or greater.')
+        return value
+
+    @validator("tol")
+    def validate_tol(cls, value):
+        if value < 0:
+            raise ValueError('tol must be greater than zero.')
+        return value
+
+    @validator("C")
+    def validate_C(cls, value):
+        if value < 0:
+            raise ValueError('C must be than zero.')
+        return value
+
+
+class SVRParameters(BaseModel):
+    epsilon: float = 0.0
+    tol: float = 1e-4
+    C: float = 1.0
+    kernel: Literal['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'] = 'rbf'
+    degree: int = 3
+    gamma: Literal['scale', 'auto'] = 'scale'
+
+    @validator("epsilon")
+    def validate_epsilon(cls, value):
+        if value < 0:
+            raise ValueError('epsilon must be 0 or greater.')
+        return value
+
+    @validator("tol")
+    def validate_tol(cls, value):
+        if value < 0:
+            raise ValueError('tol must be greater than zero.')
+        return value
+
+    @validator("C")
+    def validate_C(cls, value):
+        if value < 0:
+            raise ValueError('C must be greater than zero.')
+        return value
+
+    @validator("degree")
+    def validate_degree(cls, value):
+        if value < 0:
+            raise ValueError('degree must be 0 or greater.')
+        return value
+
+
+class MLPRegressorParameters(BaseModel):
+    hidden_layer_sizes: List[int] = [100]
+    activation : Literal['identity', 'logistic', 'tanh', 'relu'] = 'relu'
+    solver : Literal['lbfgs', 'sgd', 'adam'] = 'adam'
+    alpha: float = 0.0001
+    learning_rate : Literal['constant', 'invscaling', 'adaptive'] = 'constant'
+    learning_rate_init: float = 0.001
+    max_iter: int = 1000
+    shuffle: bool = True
+    n_iter_no_change: int = 10
+    tol: float = 1e-4
+
+    @validator("max_iter")
+    def validate_max_iter(cls, value):
+        if value <= 0:
+            raise ValueError('max_iter must be greater than zero.')
+        return value
+
+    @validator("alpha")
+    def validate_alpha(cls, value):
+        if value < 0:
+            raise ValueError('alpha must be 0 or greater.')
+        return value
+
+    @validator("tol")
+    def validate_tol(cls, value):
+        if value < 0:
+            raise ValueError('tol must be greater than zero.')
+        return value
+
+    @validator("learning_rate_init")
+    def validate_learning_rate_init(cls, value):
+        if value < 0:
+            raise ValueError('learning_rate_init must be greater than zero.')
+        return value
+
+    @validator("n_iter_no_change")
+    def validate_n_iter_no_change(cls, value):
+        if value < 1:
+            raise ValueError('n_iter_no_change must be 1 or greater.')
+        return value
+
+
 class BaggingRegressorParameters(BaseModel):
     n_estimators: int = 10
     max_samples: Union[int, float] = 1
     max_features: Union[int, float] = 1
     bootstrap: bool = True
     bootstrap_features: bool = False
-    n_jobs: Optional[int] = N_JOBS 
+    n_jobs: Optional[int] = N_JOBS
 
     @validator("n_estimators")
     def validate_n_estimators(cls, value):
@@ -56,6 +205,7 @@ class BaggingRegressorParameters(BaseModel):
             raise ValueError('max_features must be 1 or greater.')
         return value
 
+
 class ExtraTreeRegressorParameters(BaseModel):
     n_estimators: int = 100
     criterion: Literal['squared_error', 'absolute_error', 'friedman_mse', 'poisson'] = 'squared_error'
@@ -63,7 +213,7 @@ class ExtraTreeRegressorParameters(BaseModel):
     min_samples_split: int = 2
     min_samples_leaf: int = 1
     bootstrap: bool = False
-    n_jobs: Optional[int] = N_JOBS 
+    n_jobs: Optional[int] = N_JOBS
 
     @validator("n_estimators")
     def validate_n_estimators(cls, value):
@@ -88,6 +238,7 @@ class ExtraTreeRegressorParameters(BaseModel):
         if value < 1:
             raise ValueError('min_samples_leaf must be 1 or greater.')
         return value
+
 
 class GradientBoostingRegressorParameters(BaseModel):
     loss: Literal['squared_error', 'absolute_error', 'huber', 'quantile'] = 'squared_error'
@@ -128,6 +279,7 @@ class GradientBoostingRegressorParameters(BaseModel):
             raise ValueError('max_depth must be greater than zero.')
         return value
 
+
 class RandomForestRegressorParameters(BaseModel):
     n_estimators: int = 100
     criterion: Literal['squared_error', 'absolute_error', 'friedman_mse', 'poisson'] = 'squared_error'
@@ -160,11 +312,13 @@ class RandomForestRegressorParameters(BaseModel):
             raise ValueError('min_samples_leaf must be 1 or greater.')
         return value
 
+
 class LinearRegressionParameters(BaseModel):
     fit_intercept: bool = True
     copy_X: bool = True
     n_jobs: Optional[int] = N_JOBS
     positive: bool = False
+
 
 class SGDRegressorParameters(BaseModel):
     loss: Literal['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'] = 'hinge'
--- server/ml_api/apps/ml_models/utils/regression_searchers.py
+++ server/ml_api/apps/ml_models/utils/regression_searchers.py
@@ -1,38 +1,92 @@
 from hyperopt import hp
 
-
 REGRESSION_CONFIG = {
     'DecisionTreeRegressor': {
-        'criterion': hp.choice(label='criterion', options=['gini', 'entropy', 'squared_error', 'friedman_mse', 'absolute_error', 'poisson']),
+        'criterion': hp.choice(label='criterion',
+                               options=['gini', 'entropy', 'squared_error',
+                                        'friedman_mse', 'absolute_error',
+                                        'poisson']),
         'max_depth': hp.randint(label='max_depth', low=3, high=30),
+    },
+    'AdaBoostRegressor': {
+        'loss': hp.choice(label='loss',
+                          options=['linear', 'square', 'exponential']),
+        'learning_rate': hp.uniform('learning_rate', 1e-6, 1),
+        'n_estimators': hp.randint(label="n_estimators", low=50, high=300)
+    },
+    'KNeighborsRegressor': {
+        'n_neighbors': hp.randint(label="n_neighbors", low=1, high=10),
+        'weights': hp.choice(label='weights', options=['uniform', 'distance']),
+        'algorithm': hp.choice(label='algorithm',
+                               options=['auto', 'ball_tree', 'kd_tree',
+                                        'brute']),
+        'p': hp.randint(label="p", low=1, high=10)
+    },
+    'LinearSVR': {
+        'loss': hp.choice(label='loss', options=['epsilon_insensitive',
+                                                 'squared_epsilon_insensitive']),
+        'epsilon': hp.uniform('epsilon', 0, 5),
+        'C': hp.uniform('C', 0, 10)
+    },
+    'SVR': {
+        'kernel': hp.choice(label='kernel',
+                            options=['linear', 'poly', 'rbf', 'sigmoid',
+                                     'precomputed']),
+        'gamma': hp.choice(label='gamma', options=['scale', 'auto']),
+        'epsilon': hp.uniform('epsilon', 0, 5),
+        'C': hp.uniform('C', 0, 10),
+        'degree': hp.randint(label="degree", low=0, high=10)
+    },
+    'MLPRegressor': {
+        'activation': hp.choice(label='activation',
+                                options=['identity', 'logistic', 'tanh',
+                                         'relu']),
+        'solver': hp.choice(label='solver', options=['lbfgs', 'sgd', 'adam']),
+        'learning_rate': hp.choice(label='learning_rate',
+                                   options=['constant', 'invscaling',
+                                            'adaptive']),
+        'alpha': hp.uniform('alpha', 0, 1),
+        'learning_rate_init': hp.uniform('learning_rate_init', 0, 1),
     },
     'BaggingRegressor': {
         'n_estimators': hp.randint(label="n_estimators", low=10, high=100),
     },
     'ExtraTreeRegressor': {
         'n_estimators': hp.randint(label="n_estimators", low=50, high=300),
-        'criterion': hp.choice(label='criterion', options=['squared_error', 'absolute_error', 'friedman_mse', 'poisson']),
+        'criterion': hp.choice(label='criterion',
+                               options=['squared_error', 'absolute_error',
+                                        'friedman_mse', 'poisson']),
         'max_depth': hp.randint(label='max_depth', low=3, high=20),
     },
     'GradientBoostringRegressor': {
-        'loss': hp.choice(label='loss', options=['squared_error', 'absolute_error', 'huber', 'quantile']),
+        'loss': hp.choice(label='loss',
+                          options=['squared_error', 'absolute_error', 'huber',
+                                   'quantile']),
         'learning_rate': hp.uniform('learning_rate', 1e-6, 1),
         'n_estimators': hp.randint(label="n_estimators", low=50, high=300),
-        'criterion': hp.choice(label='criterion', options=['friedman_mse', 'squared_error']),
+        'criterion': hp.choice(label='criterion',
+                               options=['friedman_mse', 'squared_error']),
         'max_depth': hp.randint('max_depth', 3, 20),
     },
     'RandomForestRegressor': {
         'n_estimators': hp.randint(label='n_estimators', low=50, high=300),
-        'criterion': hp.choice(label='criterion', options=['squared_error', 'absolute_error', 'friedman_mse', 'poisson']),
+        'criterion': hp.choice(label='criterion',
+                               options=['squared_error', 'absolute_error',
+                                        'friedman_mse', 'poisson']),
         'max_depth': hp.randint(label='max_depth', low=3, high=20),
     },
     'LinearRegression': {
     },
     'SGDRegressor': {
-       'loss': hp.choice(label='loss', options=['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive']),
-       'penalty': hp.choice(label='penalty', options=['l2', 'l1', 'elasticnet', 'none']),
-       'alpha': hp.uniform('alpha', 1e-6, 1e-3),
-       'l1_ratio': hp.uniform('l1_ratio', 0.05, 0.95),
-       'learning_rate': hp.choice(label='learning_rate', options=['constant', 'optimal', 'invscaling', 'adaptive']),
+        'loss': hp.choice(label='loss', options=['squared_error', 'huber',
+                                                 'epsilon_insensitive',
+                                                 'squared_epsilon_insensitive']),
+        'penalty': hp.choice(label='penalty',
+                             options=['l2', 'l1', 'elasticnet', 'none']),
+        'alpha': hp.uniform('alpha', 1e-6, 1e-3),
+        'l1_ratio': hp.uniform('l1_ratio', 0.05, 0.95),
+        'learning_rate': hp.choice(label='learning_rate',
+                                   options=['constant', 'optimal',
+                                            'invscaling', 'adaptive']),
     },
 }
